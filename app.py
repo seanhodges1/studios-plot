8b3218e8e76e4cb41834a8091b65fc7f65ff50b5
Sean Hodges <sean.hodges.1@gmail.com>
2021-09-06 15:03:41 +1200

    Update app.py
    Added conda install instructions for specific packages needed for this script

--- app.py
+++ app.py
@@ -2,15 +2,20 @@
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
+'''
+conda create --name tsplot-3.7 python=3.7
+conda activate tsplot-3.7
+conda install -c conda-forge dash
+conda install pandas numpy
+conda install requests
+conda install xmltodict
+'''
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 import pandas as pd
-import numpy as np
-import requests
-import sys
 import web_service as ws    
 from datetime import datetime
 import datetime as dt
